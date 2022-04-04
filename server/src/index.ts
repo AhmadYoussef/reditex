380278c4a8d9229c779de12a6f24dda80f66ec82
Ahmad Youssef <ahmad.youssef@edatasystems.de>
2022-04-04 12:33:35 +0200

    My First Controller using GraphQl

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,38 @@
 import { MikroORM } from "@mikro-orm/core";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
 
   await orm.getMigrator().up();
 
-  // const post = orm.em.create(Post, { title: "My First Post" });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
+  /** creating data in Post  */
+  const post = orm.em.create(Post, { title: "My First Post" });
+  await orm.em.persistAndFlush(post);
+
+  /** get All data in Post  */
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch((err) => {
--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,30 +1,30 @@
-import { MikroORM } from "@mikro-orm/core";
+import { MikroORM, Options } from "@mikro-orm/core";
 import { Post } from "./entities/Post";
 import { __PROD__ } from "./constants";
 import path from "path";
 
-// const config: Options = {
-//   migrations: {
-//     path: path.join(__dirname, "./migrations"),
-//     pattern: /^[\w-]+\d+\.ts$/,
-//   },
-//   entities: [Post],
-//   dbName: "lireddit",
-//   type: "postgresql",
-//   user: "ahmyou",
-//   debug: !__PROD__,
-// };
-
-// export default config;
-
-export default {
+const config: Options = {
   migrations: {
     path: path.join(__dirname, "./migrations"),
-    pattern: /^[\w-]+\d+\.[tj]s$/,
+    pattern: /^[\w-]+\d+\.ts$/,
   },
   entities: [Post],
   dbName: "lireddit",
   type: "postgresql",
   user: "ahmyou",
   debug: !__PROD__,
-} as Parameters<typeof MikroORM.init>[0];
+};
+
+export default config;
+
+// export default {
+//   migrations: {
+//     path: path.join(__dirname, "./migrations"),
+//     pattern: /^[\w-]+\d+\.[tj]s$/,
+//   },
+//   entities: [Post],
+//   dbName: "lireddit",
+//   type: "postgresql",
+//   user: "ahmyou",
+//   debug: !__PROD__,
+// } as Parameters<typeof MikroORM.init>[0];
