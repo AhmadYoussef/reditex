a05950b90184600f99dba3d5314446600a086634
Ahmad Youssef <ahmad.youssef@edatasystems.de>
2022-04-04 16:52:19 +0200

    add usefull link to readme.de

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
